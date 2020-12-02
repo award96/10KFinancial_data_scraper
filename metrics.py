0a5192789a524289a7a01e39dd70acf8c3caf449
award96 <anders.s.ward@gmail.com>
2020-12-01 16:16:05 -0800

    Deleted unused files
    I may bring back some of the code from these files, but I have not needed them thusfar and they are in a different repository if I need them.

--- metrics.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-def horizontal(start, end):
-    val = end - start
-    if (start == 0) or (start == np.nan):
-        return (val, np.nan)
-    percent = val * 100 / abs(start)
-    return (val, percent)
